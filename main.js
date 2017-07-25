14d136f0a84deb272cdd7a4511f1af60c4ff29f3
Emily Deans <emilyldeans@gmail.com>
2017-07-25 17:10:46 -0400

    Created a require for the data.js and created initial HTML.

--- main.js
+++ main.js
@@ -2,15 +2,23 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const fs = require("fs");
+const DATA = require("./views/data");
 
 const mustacheExpress = require('mustache-express');
 app.engine("mustache", mustacheExpress());
 app.set("view engine", "mustache");
-app.set("views", path.join(__dirname, "public") )
+app.set("views", path.join(__dirname, "views"));
 
-const DATA = {
-  
-}
+app.get("/", (req, res) => {
+  res.render("index.mustache", DATA);
+});
+
+module.exports
+
+// To link CSS sheets
+app.get("/styles.css", (req,res) => {
+  res.send(fs.readFileSync('./styles.css'));
+});
 
 
 app.listen(3000, function() {

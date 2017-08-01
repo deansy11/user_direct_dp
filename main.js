54cbd9e58345deacb2b53b99085965afb763d9cc
Emily Deans <emilyldeans@gmail.com>
2017-08-01 14:03:32 -0400

    Added second mustache file in views.

--- main.js
+++ main.js
@@ -6,24 +6,32 @@ const app = express();
 const DATA = require("./views/data");
 
 app.engine("mustache", mustacheExpress());
-app.set("views", "./views");
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "mustache");
 
+app.use(express.static(path.join(__dirname, "public")));
+
+// To link CSS sheets check Express static notes
+app.get("./styles.css", (req,res) => {
+  res.send(fs.readFileSync('./styles.css'));
+});
+
+// express.static('public');
 app.get("/", function(req, res){
   console.log("Is working?");
   res.render("main", DATA);
 });
 
-// let userData = DATA.module.exports.users
-
-// To link CSS sheets check Express static notes
-app.use(express.static('public'));
-
-app.get("/styles.css", (req,res) => {
-  res.send(fs.readFileSync('./styles.css'));
+app.get("/users/:id", (req, res) => {
+  console.log(req.params.id);
+  let robotProfile = DATA.users.find( function(user) {
+    return user.id === req.params.id;
+  });
+  res.render("main", { users: [robotProfile]});
+  console.log(req.params.id);
 });
 
 
-app.listen(8080, function() {
-  console.log("here we are");
+app.listen(3000, () => {
+  console.log("Node running at http://localhost:3000");
 });
